810bed116c57841fe6ea4aa171af2167ab9aeab6
yjo9515 <yjo9515@naver.com>
2020-07-03 01:15:55 +0900

    java

---
+++ java/Practice/src/parctice12_8/MovieThread.java
@@ -0,0 +1,17 @@
+package parctice12_8;
+
+public class MovieThread extends Thread {
+
+	@Override
+	public void run() {
+	while(true) {
+		System.out.println("동영상을 재생합니다");
+		try {
+		Thread.sleep(1000);
+		}catch(Exception e) {
+			
+		}
+	}
+	}
+
+}
