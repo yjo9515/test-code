ea49236fb0f1e7de6f02d51406eaafa9b11d95b6
yjo9515 <yjo9515@naver.com>
2020-07-08 00:36:50 +0900

    java_pratice

---
+++ java/Practice/src/practice4/Exercise04.java
@@ -0,0 +1,24 @@
+package practice4;
+
+public class Exercise04 {
+
+	public static void main(String[] args) {
+		
+
+		while(true){
+			int math1 = (int)(Math.random()*6)+1;
+			int math2 = (int)(Math.random()*6)+1;
+			System.out.println("("+math1+", "+math2+")");
+			
+			if((math1+math2)==5) {
+				
+				break;
+			}
+				
+			}
+		
+		}
+		
+	}
+
+
---
+++ java/Practice/src/practice4/Exercise06.java
@@ -0,0 +1,18 @@
+package practice4;
+
+public class Exercise06 {
+
+	public static void main(String[] args) {
+		for(int i = 1; i<=5; i++) {
+			for(int k = 1; k<=i; k++) {
+				System.out.print("*");
+				if(i==k) {
+					System.out.println();
+				}
+			}
+		
+			
+		}
+	}
+
+}
