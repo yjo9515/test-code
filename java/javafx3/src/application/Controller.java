0953004c368d1e9ca2de55d72219094deff6c808
yjo9515 <yjo9515@naver.com>
2020-07-27 23:29:38 +0900

    javafx

--- java/javafx3/src/application/Controller.java
+++ java/javafx3/src/application/Controller.java
@@ -13,6 +13,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Controller {
@@ -22,17 +23,33 @@ public class Controller {
 	@FXML
 	private MenuItem btnLogin;
 	
+
 	public void login(ActionEvent event) {
+		try {
+			Parent login =FXMLLoader.load(getClass().getResource("view/login.fxml"));
+			Scene scene = new Scene(login);
+			Stage primaryStage = new Stage();
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("로그인");
+			primaryStage.show();
+			
+		}catch(IOException e) {
+			
+			e.printStackTrace();
+		}
 		
 	
 		
 	}
 	public void newmem(ActionEvent event)  {
 		try {
-			Parent newmem =FXMLLoader.load(getClass().getResource("view/Newmem.fxml"));
+			Parent newmem =FXMLLoader.load(getClass().getResource("view/newmem.fxml"));
 			Scene scene = new Scene(newmem);
-			Stage primaryStage = (Stage)newmem.getScene().getWindow();
+			Stage primaryStage = new Stage();
+//			Stage primaryStage = (Stage)newmem.getScene().getWindow();
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("회원가입");
+			primaryStage.show();
 			
 		}catch(IOException e) {
 			
@@ -42,10 +59,37 @@ public class Controller {
 		
 		
 	}
+   
 	public void order(ActionEvent event) {
+		try {
+			Parent order =FXMLLoader.load(getClass().getResource("view/purchase.fxml"));
+			Scene scene = new Scene(order);
+			Stage primaryStage = new Stage();
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("주문");
+			primaryStage.show();
+			
+			
+		}catch(IOException e) {
+			
+			e.printStackTrace();
+		}
+		
 		
 	}
 	public void community(ActionEvent event) {
+		Parent order;
+		try {
+			order = FXMLLoader.load(getClass().getResource("view/review.fxml"));
+			Scene scene = new Scene(order);
+			Stage primaryStage = new Stage();
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("리뷰");
+			primaryStage.show();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
 		
 	}
 	
